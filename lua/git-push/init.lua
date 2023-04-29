b11bbc77cf8659c40068d2a8dba514c9603387f7
Felipe <felipe@frontm.com>
2023-04-29 09:51:54 -0300

    feat: support native input

--- lua/git-push/init.lua
+++ lua/git-push/init.lua
@@ -3,8 +3,13 @@ GitPushConfig = GitPushConfig or {}
 
 --[[ 
 -- usage
+-- Using nui:
 -- require("git-push").show_push_dialog()
 -- this will display the dialog with the branches
+-- Using native input:
+-- local git_push = require("git-push")
+-- git_push.setup({ use_nui = false })
+-- git_push.show_push_dialog()
 ]]
 
 
@@ -30,6 +35,63 @@ local function get_git_branches()
     return branches
 end
 
+local function show_with_nui(branches)
+    local Menu = require("nui.menu")
+    local menu_items = {}
+    for _, branch in ipairs(branches) do
+        table.insert(menu_items, Menu.item(branch))
+    end
+
+    local menu = Menu({
+        position = "50%",
+        size = {
+            width = 25,
+            height = 5,
+        },
+        border = {
+            style = "rounded",
+            padding = { 1 },
+            text = {
+                top = "Choose branch to push",
+                top_align = "center",
+                bottom = "Space or Enter to push",
+            },
+        },
+        win_options = {
+            winhighlight = "Normal:Normal,FloatBorder:Normal",
+        },
+    }, {
+        lines = menu_items,
+        max_width = 20,
+        keymap = {
+            focus_next = { "j", "<Down>", "<Tab>" },
+            focus_prev = { "k", "<Up>", "<S-Tab>" },
+            close = { "<Esc>", "<C-c>" },
+            submit = { "<CR>", "<Space>" },
+        },
+        on_submit = function(item)
+            M.push_to_branch(item.text)
+        end,
+    })
+    menu:mount()
+end
+
+local function show_with_native_input(branches)
+    local prompt = ''
+    for i, branch in pairs(branches) do
+        prompt = prompt .. branch .. ' (' .. i .. ')\n'
+    end
+
+    local selected
+
+    vim.ui.input({ prompt = prompt .. 'Select: ' },
+        function(input) selected = branches[tonumber(input)] end)
+    if selected == nil then
+        return
+    end
+    M.push_to_branch(selected)
+end
+
 function M.setup(config)
     if not config then
         config = {}
@@ -39,8 +101,12 @@ function M.setup(config)
         return
     end
     GitPushConfig.is_git_repo = is_git_repo()
-    GitPushConfig.with_menu = config.use_nui or true
-    GitPushConfig.remote = config.use_nui or 'origin'
+    local use_nui = true
+    if not config.use_nui then
+        use_nui = config.use_nui
+    end
+    GitPushConfig.with_menu = use_nui
+    GitPushConfig.remote = config.origin or 'origin'
 end
 
 function M.push_to_branch(branch)
@@ -55,48 +121,16 @@ function M.show_push_dialog()
     end
 
     local branches = get_git_branches()
-    if (GitPushConfig.with_menu) then
-        local Menu = require("nui.menu")
-        local menu_items = {}
-        for _, branch in ipairs(branches) do
-            table.insert(menu_items, Menu.item(branch))
-        end
 
-        local menu = Menu({
-            position = "50%",
-            size = {
-                width = 25,
-                height = 5,
-            },
-            border = {
-                style = "rounded",
-                padding = { 1 },
-                text = {
-                    top = "Choose branch to push",
-                    top_align = "center",
-                    bottom = "Space or Enter to push",
-                },
-            },
-            win_options = {
-                winhighlight = "Normal:Normal,FloatBorder:Normal",
-            },
-        }, {
-            lines = menu_items,
-            max_width = 20,
-            keymap = {
-                focus_next = { "j", "<Down>", "<Tab>" },
-                focus_prev = { "k", "<Up>", "<S-Tab>" },
-                close = { "<Esc>", "<C-c>" },
-                submit = { "<CR>", "<Space>" },
-            },
-            on_submit = function(item)
-                M.push_to_branch(item.text)
-            end,
-        })
-        menu:mount()
+    if (GitPushConfig.with_menu) then
+        show_with_nui(branches)
+    else
+        show_with_native_input(branches)
     end
 end
 
 M.setup()
 
+
 return M
+
